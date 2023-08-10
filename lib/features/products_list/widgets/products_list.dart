0845d07497fdf6f0914766aa6337ca629a98e157
Musab Nasredlin <musabagab2010@gmail.com>
2023-08-11 00:11:45 +0200

    created vertical and horizontal list for products list

--- lib/features/products_list/widgets/products_list.dart
+++ lib/features/products_list/widgets/products_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_coding_test/models/product.dart';
 import 'package:flutter_coding_test/ui/app_colors.dart';
 import 'package:flutter_coding_test/ui/custom_text.dart';
 
@@ -20,80 +19,141 @@ class _ProductsListState extends State<ProductsList> {
     return BlocBuilder<ProductBloc, ProductsScreenState>(
         builder: (context, state) {
       return Expanded(
-        child: ListView.builder(
-          itemCount: state.products.length,
-          scrollDirection: Axis.vertical,
-          itemBuilder: (context, index) {
-            final product = state.products[index];
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
+        child: state.products.isEmpty
+            ? const Center(
+                child: MediumText(text: "لا توجد منتجات"),
+              )
+            : state.viewMode == 0
+                ? verticalProductsList(state)
+                : horizontalProductsList(state),
+      );
+    });
+  }
+
+  ListView verticalProductsList(ProductsScreenState state) {
+    return ListView.builder(
+      itemCount: state.products.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) {
+        final product = state.products[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const SizedBox(height: 10),
+                    MediumText(
+                      text: product.name,
+                      fontSize: 18,
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        const SizedBox(height: 10),
+                        const RegularText(
+                          text: "دولار",
+                          fontSize: 12,
+                        ),
+                        const SizedBox(width: 5),
                         MediumText(
-                          text: product.name,
-                          fontSize: 18,
+                          text: product.price.toStringAsFixed(0),
+                          fontColor: AppColors.green,
+                          fontSize: 20,
                         ),
-                        const SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            const RegularText(
-                              text: "دولار",
-                              fontSize: 12,
-                            ),
-                            const SizedBox(width: 5),
-                            MediumText(
-                              text: product.price.toStringAsFixed(0),
-                              fontColor: AppColors.green,
-                              fontSize: 20,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Container(
-                          width: 65,
-                          height: 30,
-                          decoration: BoxDecoration(
-                            color: AppColors.lightGrey,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: LightText(
-                              text: product.storeName,
-                              fontSize: 10,
-                              fontColor: AppColors.darkGrey,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10), // Add spacing here
                       ],
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  Container(
-                    width: 115,
-                    height: 115,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      image: DecorationImage(
-                        image: AssetImage(product.imageUrl),
-                        fit: BoxFit.cover,
+                    const SizedBox(height: 10),
+                    Container(
+                      width: 65,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: AppColors.lightGrey,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: LightText(
+                          text: product.storeName,
+                          fontSize: 10,
+                          fontColor: AppColors.darkGrey,
+                        ),
                       ),
                     ),
+                    const SizedBox(height: 10), // Add spacing here
+                  ],
+                ),
+              ),
+              const SizedBox(width: 16),
+              Container(
+                width: 115,
+                height: 115,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: AssetImage(product.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  GridView horizontalProductsList(ProductsScreenState state) {
+    return GridView.builder(
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: state.products.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) {
+        final product = state.products[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(width: 16),
+              Container(
+                height: 115,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: AssetImage(product.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              MediumText(
+                text: product.name,
+                fontSize: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const RegularText(
+                    text: "دولار",
+                    fontSize: 12,
+                  ),
+                  const SizedBox(width: 5),
+                  MediumText(
+                    text: product.price.toStringAsFixed(0),
+                    fontColor: AppColors.green,
+                    fontSize: 20,
                   ),
                 ],
               ),
-            );
-          },
-        ),
-      );
-    });
+            ],
+          ),
+        );
+      },
+    );
   }
 }
